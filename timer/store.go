f3b863abdfd8d8e1fa1d386aecb18f9743e1575b
Geraldo Fada <geraldofada.neto@gmail.com>
2020-01-02 20:02:10 -0300

    New functions to work with files

--- timer/store.go
+++ timer/store.go
@@ -1,1 +1,67 @@
 package timer
+
+import (
+	"encoding/gob"
+	"os"
+)
+
+// Save stores a list of timers in a bin file
+func Save(t []*Timer, path string) error {
+	file, err := os.OpenFile(path, os.O_CREATE|os.O_WRONLY, 0666)
+	defer file.Close()
+	if err != nil {
+		return err
+	}
+
+	enc := gob.NewEncoder(file)
+
+	err = enc.Encode(t)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// Read returns a struct timer from a given bin file
+func Read(id int, path string) (*Timer, error) {
+	file, err := os.Open(path)
+	defer file.Close()
+	if err != nil {
+		return nil, err
+	}
+
+	var t []*Timer
+	dec := gob.NewDecoder(file)
+	err = dec.Decode(&t)
+	if err != nil {
+		return nil, err
+	}
+
+	return t[id], nil
+}
+
+// Remove removes a Timer from a given bin file
+func Remove(id int, path string) error {
+	file, err := os.Open(path)
+	defer file.Close()
+	if err != nil {
+		return nil
+	}
+
+	var t []*Timer
+	dec := gob.NewDecoder(file)
+	err = dec.Decode(&t)
+	if err != nil {
+		return err
+	}
+
+	t = append(t[:id], t[id+1:]...)
+
+	err = Save(t, path)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
